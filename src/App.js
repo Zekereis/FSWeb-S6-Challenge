a7e2ca7fc9a9e4151afbb15833f6086d0b1170c1
Zekereis <zekereisoglu@outlook.com>
2022-12-05 22:01:49 +0300

    ödev

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React ,{useEffect, useState} from 'react';
+import axios from "axios";
+import Karakter from './components/Karakter';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [chars, setChars] = useState(null);
+  const [visibleIndex, setVisibleIndex] = useState(2);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+    .then(res => setChars(res.data));
+  }, [])
+  
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {
+        chars === null
+        ? <div>Yükleniyor</div>
+        : <div className='chars-list'>
+          {chars.map((k ,sira) => <Karakter kisi = {k} kisiIndex={sira} acikMi={sira === visibleIndex} tiklayincaYap = {() => setVisibleIndex(sira)}/>)}
+        </div>
+      }
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,24 @@
 // Karakter bileşeniniz buraya gelecek
+import React, { useState } from "react";
+
+const Karakter = (prop) => {
+const [acik, setAcik] = useState(false);
+
+
+    return (
+        <div onClick={prop.tiklayincaYap} className='char' key={prop.kisi.url}>
+        <h2>{prop.kisi.name}</h2> 
+        {prop.acikMi && (
+            <div className='char-ozellikleri'>
+            <div>Göz rengi: {prop.kisi.eye_color}</div>
+            <div>Boyu: {prop.kisi.height}</div>
+            <div>Kilosu: {prop.kisi.mass}</div>
+            <div>Doğum tarihi: {prop.kisi.birth_year}</div>
+          </div>
+        )}
+         
+         </div>
+    )
+}
+
+export default Karakter;
